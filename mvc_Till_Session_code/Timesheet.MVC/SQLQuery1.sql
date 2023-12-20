2a6331b0fd413582dd3a32ad4feaefa7d54e454f
Piyush-rbc-repo <piyush.kundalkar@kiya.ai>
2023-12-20 18:49:00 +0530

    session_timeout and color change

---
+++ mvc_Till_Session_code/Timesheet.MVC/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿Select *from Memberships
+
+exec usp_AddUpdateLookupMaster 'R',null,null,'5'
+
+
+Select  * from tbl_LookupMaster where s_MasterCode like '%Ashish%'
+Select distinct s_value1 from tbl_LookupMaster 
+
+Select *From tbl_LookupMaster Where s_MasterCode='Sreeram.Kumar'
+
+Select count(1) From tbl_LookupMaster where s_value3='Jersey'
+
+
