941a1128d6d062271d9bf908ea0b64dc947895a2
Piyush-rbc-repo <piyush.kundalkar@kiya.ai>
2023-12-21 13:04:14 +0530

    task and session combine

---
+++ Timesheet.MVC/Timesheet.MVC/Filters/AuthorizeRole.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Timesheet.Service.ServiceInterface;
+
+namespace Timesheet.MVC.Filters
+{
+    public sealed class UserRole
+    {
+        private UserRole() { }
+
+        public const string Manager = "Project Manager";
+        public const string Admin = "Admin";
+        public const string Developer = "Senior Developer";
+        public const string Default = "Default";
+    }
+    public class AuthorizeRole : AuthorizeAttribute
+    {
+        string[] UserRoles;
+        private IUserService _IUserService;
+
+        public AuthorizeRole(params string[] roles)
+        {
+            UserRoles = roles;
+            _IUserService = new Timesheet.Service.ServiceLibrary.UserService();
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (base.AuthorizeCore(httpContext))
+            {
+                //if (!UserRoles.Any())
+                //    return true;
+
+                if (Array.Exists(UserRoles, E => E == httpContext.Session["IRole"].ToString()))
+                {
+                    return true;
+                }
+
+        private bool CheckPageAccess(string[] UserRoles, HttpContextBase httpContext)
+        {
+            return UserRoles.Any(x => x == httpContext.Session["IRole"].ToString())
+                || UserRoles.Any(x => x == UserRole.Default);
+        }
+
+        protected override void HandleUnauthorizedRequest(
+          AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                    {
+                        { "Controller", "Account" },
+                        { "Action", "Logout" },
+                        {"DisplayMessage","Session Timed out! Please Login Again!" }
+                    });//()
+        }
+    }
+}
